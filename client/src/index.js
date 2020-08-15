5ddbdcc334513128b202c624d3a54d5dc1c8c7a1
hojin9622 <kiss0104040@gmail.com>
2020-08-15 13:36:38 +0900

    Add redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+const createStoreWithMiddleware = applyMiddleware(
+    promiseMiddleware,
+    ReduxThunk
+)(createStore);
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider
+            store={createStoreWithMiddleware(
+                Reducer,
+                window.__REDUX_DEVTOOLS_EXTENSION__ &&
+                    window.__REDUX_DEVTOOLS_EXTENSION__()
+            )}
+        >
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
