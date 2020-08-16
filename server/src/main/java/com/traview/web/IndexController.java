708139193490b973e192d1485ae4b36d1fe7d7a6
jungdahee <jungdahee010@naver.com>
2020-08-16 19:15:06 +0900

    Add Login Function

--- server/src/main/java/com/traview/web/IndexController.java
+++ server/src/main/java/com/traview/web/IndexController.java
@@ -1,22 +1,34 @@
 package com.traview.web;
 
+import com.traview.config.oauth.dto.SessionUser;
 import com.traview.service.PostsService;
 import com.traview.web.dto.PostsResponseDto;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
